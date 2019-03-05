c5f49c951df8c0ad7672214ea91e2e90d3f4cda1
einarf <eforselv@gmail.com>
2019-03-05 02:16:01 +0100

    Major attribute cleanup in BaseWindow

--- moderngl_window/base/window.py
+++ moderngl_window/base/window.py
@@ -1,8 +1,19 @@
+from functools import wraps
 import sys
 from typing import Any, Tuple
 
 import moderngl
 from moderngl_window.base import WindowConfig
+
+
+def require_callable(func):
+    """Decorator ensuring assigned callbacks are valid callables"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not callable(args[1]):
+            raise ValueError("{} is not a callable".format(args[1]))
+        return func(*args, **kwargs)
+    return wrapper
 
 
 class BaseWindow:
@@ -26,42 +37,114 @@ class BaseWindow:
             cursor (bool): Enable/disable displaying the cursor inside the window
         """
         # Window parameters
-        self.title = title
-        self.gl_version = gl_version
-        self.width, self.height = size
-        self.resizable = resizable
-        self.buffer_width, self.buffer_height = size
-        self.fullscreen = fullscreen
-        self.vsync = vsync
-        self.aspect_ratio = aspect_ratio
-        self.samples = samples
-        self.cursor = cursor
+        self._title = title
+        self._gl_version = gl_version
+        self._width, self._height = size
+        self._resizable = resizable
+        self._buffer_width, self._buffer_height = size
+        self._fullscreen = fullscreen
+        self._vsync = vsync
+        self._aspect_ratio = aspect_ratio
+        self._samples = samples
+        self._cursor = cursor
 
         # Callback functions
-        self.render_func = dummy_func
-        self.resize_func = dummy_func
-        self.key_event_func = dummy_func
-        self.mouse_position_event_func = dummy_func
-        self.mouse_press_event_func = dummy_func
-        self.mouse_release_event_func = dummy_func
+        self._render_func = dummy_func
+        self._resize_func = dummy_func
+        self._key_event_func = dummy_func
+        self._mouse_position_event_func = dummy_func
+        self._mouse_press_event_func = dummy_func
+        self._mouse_release_event_func = dummy_func
 
         # Internal states
-        self.ctx = None  # type: moderngl.Context
-        self.frames = 0  # Frame counter
+        self._ctx = None  # type: moderngl.Context
+        self._viewport = None
+        self._frames = 0  # Frame counter
         self._close = False
         self._config = None
 
         if not self.keys:
-            raise ValueError("Window {} class missing keys".format(self.__class__))
+            raise ValueError("Window class {} missing keys attribute".format(self.__class__))
+
+    @property
+    def title(self) -> str:
+        """str: Window title"""
+        return self._title
+
+    @property
+    def gl_version(self) -> Tuple[int, int]:
+        """(major, minor): Required OpenGL version"""
+        return self._gl_version
+
+    @property
+    def width(self) -> int:
+        """int: The current window width"""
+        return self._width
+    
+    @property
+    def height(self) -> int:
+        """int: The current window height"""
+        return self._height
+
+    @property
+    def size(self) -> Tuple[int, int]:
+        """
+        (width, height): current window size
+        """
+        return self._width, self._height
+
+    @property
+    def buffer_size(self) -> Tuple[int, int]:
+        """
+        Returns: (with, heigh) tuple with the current window buffer size
+        """
+        return self._buffer_width, self._buffer_height
+
+    @property
+    def viewport(self) -> Tuple[int, int, int, int]:
+        """(int, int, int, int): current window viewport"""
+        return self._viewport
+
+    @property
+    def resizable(self) -> bool:
+        """bool: Window is resizable"""
+        return self._resizable
+
+    @property
+    def fullscreen(self) -> bool:
+        """bool: Window is in fullscreen mode"""
+        return self._fullscreen
 
     @property
     def config(self) -> WindowConfig:
         """Get the current WindowConfig instance"""
         return self._config
 
+    @property
+    def vsync(self) -> bool:
+        """bool: vertical sync enabled/disabled"""
+        return self._vsync
+
+    @property
+    def aspect_ratio(self) -> float:
+        """float: Aspect ratio configured for the viewport"""
+        return self._aspect_ratio
+
+    @property
+    def samples(self) -> float:
+        """float: Number of Multisample anti-aliasing (MSAA) samples"""
+        return self._samples
+
+    @property
+    def cursor(self) -> bool:
+        return self._cursor
+
     @config.setter
-    def config(self, config):
-        """Set up the WindowConfig instance"""
+    def config(self, config) -> None:
+        """
+        Set up the WindowConfig instance.
+        A WindowConfig class is not required, but callback methods must be mapped.
+        """
         self.render_func = getattr(config, 'render', dummy_func)
         self.resize_func = getattr(config, 'resize', dummy_func)
         self.key_event_func = getattr(config, 'key_event', dummy_func)
@@ -72,35 +155,76 @@ class BaseWindow:
         self._config = config
 
     @property
-    def size(self) -> Tuple[int, int]:
-        """
-        Returns: (width, height) tuple with the current window size
-        """
-        return self.width, self.height
+    def render_func(self):
+        return self._render_func
+
+    @render_func.setter
+    @require_callable
+    def render_func(self, func):
+        self._render_func = func
 
     @property
-    def buffer_size(self) -> Tuple[int, int]:
-        """
-        Returns: (with, heigh) tuple with the current window buffer size
-        """
-        return self.buffer_width, self.buffer_height
+    def resize_func(self):
+        return self._resize_func
+
+    @resize_func.setter
+    @require_callable
+    def resize_func(self, func):
+        self._resize_func = func
+
+    @property
+    def key_event_func(self):
+        return self._key_event_func
+
+    @key_event_func.setter
+    @require_callable
+    def key_event_func(self, func):
+        self._key_event_func = func
+
+    @property
+    def mouse_position_event_func(self):
+        return self._mouse_position_event_func
+
+    @mouse_position_event_func.setter
+    @require_callable
+    def mouse_position_event_func(self, func):
+        self._mouse_position_event_func = func
+
+    @property
+    def mouse_press_event_func(self):
+        return self._mouse_press_event_func
+
+    @mouse_press_event_func.setter
+    @require_callable
+    def mouse_press_event_func(self, func):
+        self._mouse_press_event_func = func
+
+    @property
+    def mouse_release_event_func(self):
+        return self._mouse_release_event_func
+
+    @mouse_release_event_func.setter
+    @require_callable
+    def mouse_release_event_func(self, func):
+        self._mouse_release_event_func = func
+
+    @property
+    def ctx(self) -> moderngl.Context:
+        """moderngl.Context: The ModernGL context for the window"""
+        return self._ctx
 
     @property
     def is_closing(self) -> bool:
-        """
-        Returns: (bool) Is the window about to close?
-        """
+        """(bool) Is the window about to close?"""
         return self._close
 
     def close(self) -> None:
-        """
-        Signal for the window to close
-        """
+        """Signal for the window to close"""
         self._close = True
 
     def render(self, time: float, frame_time: float) -> None:
         """
-        Renders a frame
+        Renders a frame by calling the configured render callback
 
         Args:
             time (float): Current time in seconds
@@ -110,7 +234,7 @@ class BaseWindow:
 
     def swap_buffers(self) -> None:
         """
-        Library specific buffer swap method
+        Library specific buffer swap method. Must be overridden.
         """
         raise NotImplementedError()
 
@@ -119,8 +243,8 @@ class BaseWindow:
         Should be called every time window is resized
         so the example can adapt to the new size if needed
         """
-        if self.config:
-            self.config.resize(width, height)
+        if self._resize_func:
+            self._resize_func(width, height)
 
     def destroy(self) -> None:
         """
@@ -137,25 +261,27 @@ class BaseWindow:
         If aspect ratio is None the viewport will be scaled
         to the entire window size regardless of size.
         """
-        if self.aspect_ratio:
-            expected_width = int(self.buffer_height * self.aspect_ratio)
-            expected_height = int(expected_width / self.aspect_ratio)
+        if self._aspect_ratio:
+            expected_width = int(self._buffer_height * self._aspect_ratio)
+            expected_height = int(expected_width / self._aspect_ratio)
 
-            if expected_width > self.buffer_width:
-                expected_width = self.buffer_width
-                expected_height =  int(expected_width / self.aspect_ratio)
+            if expected_width > self._buffer_width:
+                expected_width = self._buffer_width
+                expected_height =  int(expected_width / self._aspect_ratio)
 
-            blank_space_x = self.buffer_width - expected_width
-            blank_space_y = self.buffer_height - expected_height
+            blank_space_x = self._buffer_width - expected_width
+            blank_space_y = self._buffer_height - expected_height
 
-            self.ctx.viewport = (
+            self._viewport = (
                 blank_space_x // 2,
                 blank_space_y // 2,
                 expected_width,
                 expected_height,
             )
         else:
-            self.ctx.viewport = (0, 0, self.buffer_width, self.buffer_height)
+            self._viewport = (0, 0, self._buffer_width, self._buffer_height)
+
+        self._ctx.viewport = self._viewport
 
     @property
     def gl_version_code(self) -> int:
@@ -171,12 +297,12 @@ class BaseWindow:
         """
         print("Context Version:")
         print('ModernGL:', moderngl.__version__)
-        print('vendor:', self.ctx.info['GL_VENDOR'])
-        print('renderer:', self.ctx.info['GL_RENDERER'])
-        print('version:', self.ctx.info['GL_VERSION'])
+        print('vendor:', self._ctx.info['GL_VENDOR'])
+        print('renderer:', self._ctx.info['GL_RENDERER'])
+        print('version:', self._ctx.info['GL_VERSION'])
         print('python:', sys.version)
         print('platform:', sys.platform)
-        print('code:', self.ctx.version_code)
+        print('code:', self._ctx.version_code)
 
 
 def dummy_func(*args, **kwargs) -> None:
