7ffd86273386fe829e7bf15aec799df24ec2b28f
einarf <eforselv@gmail.com>
2019-03-07 20:54:47 +0100

    Base timer class

---
+++ moderngl_window/timers/base.py
@@ -0,0 +1,68 @@
+
+class BaseTimer:
+    """
+    A timer controls the time passed into the the render function.
+    This can be used in creative ways to control the current time
+    such as basing it on current location in an audio file.
+
+    A core feature of a timer is being able to:
+
+    * Pause the current 
+
+    All methods must be implemented.
+    """
+
+    @property
+    def is_paused(self) -> bool:
+        """The pause state of the timer"""
+        raise NotImplementedError()
+
+    @property
+    def is_running(self) -> bool:
+        """Is the timer currently running?"""
+        raise NotImplementedError()
+
+    @property
+    def time(self) -> float:
+        """
+        Get the current time in seconds
+
+        Returns:
+            The current time in seconds
+        """
+        raise NotImplementedError()
+
+    @time.setter
+    def time(self, value: float):
+        """
+        Set the current time in seconds.
+
+        Args:
+            value (float): The new time
+        """
+        raise NotImplementedError()
+
+    def start(self):
+        """
+        Start the timer initially or resume after pause
+        Raises:
+            NotImplementedError
+        """
+        raise NotImplementedError()
+
+    def pause(self):
+        """Pause the timer"""
+        raise NotImplementedError()
+
+    def toggle_pause(self):
+        """Toggle pause state"""
+        raise NotImplementedError()
+
+    def stop(self) -> float:
+        """
+        Stop the timer. Should only be called once when stopping the timer.
+
+        Returns:
+            The time the timer was stopped
+        """
+        raise NotImplementedError()
