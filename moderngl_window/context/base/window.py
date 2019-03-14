63cfaffeda42a102f374940a61e54b6022ce38e6
Einar Forselv <eforselv@gmail.com>
2019-03-14 05:57:39 +0100

    Do not allow resizing in fullscreen mode

--- moderngl_window/context/base/window.py
+++ moderngl_window/context/base/window.py
@@ -68,6 +68,10 @@ class BaseWindow:
         self._key_pressed_map = {}
         self._modifiers = KeyModifiers
 
+        # Do not allow resize in fullscreen
+        if self.fullscreen:
+            self.resizable = False
+
         if not self.keys:
             raise ValueError("Window class {} missing keys attribute".format(self.__class__))
 
