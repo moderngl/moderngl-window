d9064ca584e0efb5ae632f05ee13c49ade7b5e60
einarf <eforselv@gmail.com>
2019-07-06 04:04:30 +0200

    Format tweak

--- moderngl_window/resources/tracks.py
+++ moderngl_window/resources/tracks.py
@@ -6,6 +6,7 @@ from rocket.tracks import Track
 
 class Tracks:
     """Registry for requested rocket tracks"""
+
     def __init__(self):
         self.tacks = []
         self.track_map = {}
@@ -19,10 +20,12 @@ class Tracks:
         """
         name = name.lower()
         track = self.track_map.get(name)
+
         if not track:
             track = Track(name)
             self.tacks.append(track)
             self.track_map[name] = track
+
         return track
 
 
