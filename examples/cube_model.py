1e1d7cba85e64962365ed73b1e9094cce066051f
einarf <eforselv@gmail.com>
2019-07-06 20:42:11 +0200

    Simple obj model render

--- examples/cube_model.py
+++ examples/cube_model.py
@@ -4,9 +4,10 @@ from pyrr import Matrix44, matrix44, Vector3
 import moderngl
 import moderngl_window as mglw
 from moderngl_window import resources
-from moderngl_window.resources.meta import SceneDescription
-
-from moderngl_window.conf import settings
+from moderngl_window.resources.meta import (
+    SceneDescription,
+    TextureDescription,
+)
 
 mglw.register_resource_dir(Path(__file__).parent / 'resources')
 
@@ -16,9 +17,24 @@ class CubeModel(mglw.WindowConfig):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cube = resources.scenes.load(SceneDescription(path='scenes/crate.obj', label='crate'))
+        # self.texture = resources.textures.load(TextureDescription(path='textures/crate.png', label='crate'))
 
     def render(self, time, frametime):
-        pass
+        """Render the scene"""
+        self.ctx.enable_only(moderngl.DEPTH_TEST | moderngl.CULL_FACE)
+
+        proj = matrix44.create_perspective_projection(75, self.wnd.aspect_ratio, 0.1, 100)
+
+        # Create camera matrix with rotation and translation
+        translation = matrix44.create_from_translation((0, 0, -1.5))
+        rotation = matrix44.create_from_eulers((time, time, time))
+        camera = matrix44.multiply(rotation, translation)
+
+        self.cube.draw(
+            projection_matrix=proj,
+            camera_matrix=camera,
+            time=time,
+        )
 
 
 if __name__ == '__main__':
