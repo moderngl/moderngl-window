3a2baef368ca45a989afc916ae628f5d5fb5233b
einarf <eforselv@gmail.com>
2019-07-09 09:08:00 +0200

    decorator for changing texture dirs

---
+++ moderngl_window/resources/decorators.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from typing import List, Union
+
+from contextlib import contextmanager
+from moderngl_window.conf import settings
+
+
+@contextmanager
+def texture_dirs(paths: List[Union[Path, str]]):
+    """Context manager temporarily replacing texture paths
+    Args:
+        paths (List[Union[Path, str]]): list of paths
+    """
+    original_dirs = settings.DATA_DIRS
+    settings.TEXTURE_DIRS = paths
+    yield None
+    settings.TEXTURE_DIRS =  original_dirs
