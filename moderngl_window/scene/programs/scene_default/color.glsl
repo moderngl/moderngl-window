9161f80db6152552ea5b06d55d116df3aa9f1fc2
Einar Forselv <eforselv@gmail.com>
2019-07-11 19:14:17 +0200

    color.glsl comments

--- moderngl_window/scene/programs/scene_default/color.glsl
+++ moderngl_window/scene/programs/scene_default/color.glsl
@@ -6,6 +6,8 @@ in vec3 in_position;
 in vec3 in_normal;
 
 uniform mat4 m_proj;
+// Use separate model and camera matrix. This means we don't have
+// to calculate modelview matrix in python every frame
 uniform mat4 m_view;
 uniform mat4 m_cam;
 
@@ -15,16 +17,19 @@ out vec3 pos;
 void main() {
     mat4 mv = m_cam * m_view;
     vec4 p = mv * vec4(in_position, 1.0);
-	gl_Position = m_proj * p;
+    gl_Position = m_proj * p;
+    // Calculating the normal matrix in the vertex shader
+    // means we don't have to do this expensive calculation in python
     mat3 m_normal = transpose(inverse(mat3(mv)));
     normal = m_normal * in_normal;
+    // Pass to position to fragment shader so we get an interpolated
+    // position over the entire triangle for per pixel lighting
     pos = p.xyz;
 }
 
 #elif defined FRAGMENT_SHADER
 
 out vec4 fragColor;
-uniform float time;
 uniform vec4 color;
 
 in vec3 normal;
@@ -32,8 +37,9 @@ in vec3 pos;
 
 void main()
 {
-    vec3 dir = normalize(-pos); //vec3(0.0, 0.0, 1.0);
-    float l = dot(dir, normalize(normal));
+    // Just use the camera as the only light source
+    float l = dot(normalize(-pos), normalize(normal));
+    // 25% ambient, 75% light
     fragColor = color * (0.25 + abs(l) * 0.75);
 }
 
