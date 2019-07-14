56662c77c04b01247e0d5849f4bd8eec3abbb435
einarf <eforselv@gmail.com>
2019-07-14 04:21:03 +0200

    Include gltf model testing

--- examples/cube_model.py
+++ examples/cube_model.py
@@ -10,14 +10,31 @@ from moderngl_window.resources.meta import (
 from moderngl_window.scene.camera import KeyboardCamera
 
 resources.register_dir((Path(__file__).parent / 'resources').resolve())
+# resources.register_dir(r'C:\Users\efors\Documents\ModernGL\glTF-Sample-Models\2.0')
 
 
 class CubeModel(mglw.WindowConfig):
+    # window_size = (1920, 1080)
+    aspect_ratio = 3440 / 1440
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.cube = resources.scenes.load(SceneDescription(path='scenes/crate.obj'))
-        self.camera = KeyboardCamera(self.wnd.keys, fov=75.0, aspect=self.wnd.aspect_ratio, near=0.1, far=100)
+        self.scene = resources.scenes.load(SceneDescription(path='scenes/crate.obj'))
+
+        # Test models from: https://github.com/KhronosGroup/glTF-Sample-Models/tree/master/2.0
+        # self.scene = resources.scenes.load(SceneDescription(path='2CylinderEngine/glTF-Binary/2CylinderEngine.glb'))
+        # self.scene = resources.scenes.load(SceneDescription(path='CesiumMilkTruck/glTF-Embedded/CesiumMilkTruck.gltf'))
+        # self.scene = resources.scenes.load(SceneDescription(path='CesiumMilkTruck/glTF-Binary/CesiumMilkTruck.glb'))
+        # self.scene = resources.scenes.load(SceneDescription(path='CesiumMilkTruck/glTF/CesiumMilkTruck.gltf'))
+        # self.scene = resources.scenes.load(SceneDescription(path='Sponza/glTF/Sponza.gltf'))
+        # self.scene = resources.scenes.load(SceneDescription(path='Lantern/glTF-Binary/Lantern.glb'))
+        # self.scene = resources.scenes.load(SceneDescription(path='Buggy/glTF-Binary/Buggy.glb'))
+        # self.scene = resources.scenes.load(SceneDescription(path='VC/glTF-Binary/VC.glb'))
+        # self.scene = resources.scenes.load(SceneDescription(path='DamagedHelmet/glTF-Binary/DamagedHelmet.glb'))
+
+        self.camera = KeyboardCamera(self.wnd.keys, fov=75.0, aspect=self.wnd.aspect_ratio, near=0.1, far=1000.0)
+        # Use this for gltf scenes for better camera controls
+        # self.camera.velocity = self.scene.diagonal_size / 5.0
 
     def render(self, time: float, frametime: float):
         """Render the scene"""
@@ -26,11 +43,12 @@ class CubeModel(mglw.WindowConfig):
         # Create camera matrix with rotation and translation
         translation = matrix44.create_from_translation((0, 0, -1.5))
         rotation = matrix44.create_from_eulers((time, time, time))
+        # rotation = matrix44.create_from_eulers((0, 0, 0))
         model_matrix = matrix44.multiply(rotation, translation)
 
-        camera_matrix = matrix44.multiply(model_matrix, self.camera.matrix)       
+        camera_matrix = matrix44.multiply(model_matrix, self.camera.matrix)
 
-        self.cube.draw(
+        self.scene.draw(
             projection_matrix=self.camera.projection.matrix,
             camera_matrix=camera_matrix,
             time=time,
