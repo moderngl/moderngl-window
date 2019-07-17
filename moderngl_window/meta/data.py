ef137e7f74bc455293a7dc9363c1f81242a6b140
einarf <eforselv@gmail.com>
2019-07-17 22:37:39 +0200

    Data loaders should not have a default kind

--- moderngl_window/meta/data.py
+++ moderngl_window/meta/data.py
@@ -3,7 +3,7 @@ from moderngl_window.meta.base import ResourceDescription
 
 class DataDescription(ResourceDescription):
     """Describes data file to load"""
-    default_kind = 'binary'
+    default_kind = None
     resource_type = 'data'
 
     def __init__(self, path=None, kind=None, **kwargs):
