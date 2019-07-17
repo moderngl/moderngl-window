358dabda6261fd3f07f212df20da28748b8f5094
einarf <eforselv@gmail.com>
2019-07-17 22:38:29 +0200

    Extend data loader test

--- tests/test_loaders_data.py
+++ tests/test_loaders_data.py
@@ -9,17 +9,27 @@ resources.register_dir((Path(__file__).parent / 'fixtures' / 'resources').resolv
 
 class DataLoaderTestcase(TestCase):
 
-    def test_binary(self):
+    def test_txt(self):
+        """Ensure correct loader is selected by looking at file extension (txt)"""
+        text = resources.data.load(DataDescription(path='data/data.txt'))
+        self.assertEqual(text, "Hello")
+
+    def test_json(self):
+        """Ensure correct loader is selected by looking at file extension (json)"""
+        json = resources.data.load(DataDescription(path='data/data.json'))
+        self.assertEqual(json, {"test": "Hello"})
+
+    def test_binary_kind(self):
         """Loading a binary file"""
         data = resources.data.load(DataDescription(path='data/data.bin', kind="binary"))
         self.assertEqual(data, b'Hello')
 
-    def test_text(self):
+    def test_text_kind(self):
         """Load a e textfile"""
         text = resources.data.load(DataDescription(path='data/data.txt', kind="text"))
         self.assertEqual(text, "Hello")
 
-    def test_json(self):
+    def test_json_kind(self):
         """Load a json file"""
         json = resources.data.load(DataDescription(path='data/data.json', kind="json"))
         self.assertEqual(json, {"test": "Hello"})
