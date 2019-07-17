29815d02046154c019a46ee41c75c28e25991384
einarf <eforselv@gmail.com>
2019-07-17 23:13:21 +0200

    Separate AttributeName tests

---
+++ tests/test_attribute_names.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from moderngl_window.geometry import AttributeNames
+
+
+class AttributeNamesTestCase(TestCase):
+
+    def test_attributes(self):
+        """Ensure we can create an instance with partial overrides"""
+        attrs = AttributeNames(
+            position="vertex",
+            normal="normals",
+        )
+        self.assertEqual(attrs.POSITION, "vertex")
+        self.assertEqual(attrs.NORMAL, "normals")
+        self.assertEqual(attrs.TANGENT, "in_tangent")
