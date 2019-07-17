6f3f60e26cb7f0ab666bf1a97babe96d1235850f
einarf <eforselv@gmail.com>
2019-07-17 04:28:43 +0200

    Make quad methods available in geometry module

--- moderngl_window/geometry/__init__.py
+++ moderngl_window/geometry/__init__.py
@@ -2,3 +2,4 @@ from moderngl_window.geometry.attributes import AttributeNames
 from moderngl_window.geometry.cube import cube
 from moderngl_window.geometry.bbox import bbox
 from moderngl_window.geometry.sphere import sphere
+from moderngl_window.geometry.quad import quad_2d, quad_fs
