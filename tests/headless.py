62c88924b2445f7f321313f94c18e2f8e9f61af2
einarf <eforselv@gmail.com>
2019-07-18 23:26:01 +0200

    Make headless testcase base more configurable

--- tests/headless.py
+++ tests/headless.py
@@ -7,11 +7,16 @@ from moderngl_window.conf import settings
 
 class HeadlessTestCase(TestCase):
     """Test using a headless window/context"""
+    window_size = (16, 16)
+    aspect_ratio = 1.0
 
     @classmethod
     def setUpClass(cls):
         """Create a headless window and activate the context"""
         settings.WINDOW['class'] = 'moderngl_window.context.headless.Window'
+        settings.WINDOW['size'] = cls.window_size
+        settings.WINDOW['aspect_ratio'] = cls.aspect_ratio
+
         cls.window = moderngl_window.create_window_from_settings()
         moderngl_window.activate_context(window=cls.window)
 
