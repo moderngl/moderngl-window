506d535b8639f36f5c7350e3e492627dd6b62f44
Einar Forselv <eforselv@gmail.com>
2019-07-24 00:21:48 +0200

    Test texture loaders

---
+++ tests/test_loaders_texture.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from unittest import TestCase
+
+from headless import HeadlessTestCase
+from moderngl_window import resources
+from moderngl_window.meta import TextureDescription
+
+resources.register_dir((Path(__file__).parent / 'fixtures' / 'resources').resolve())
+
+
+class TextureLoadersTestCase(HeadlessTestCase):
+    window_size = (16, 16)
+    aspect_ratio = 1.0
+
+    def test_texture_2d(self):
+        texture = resources.textures.load(TextureDescription(path='textures/crate.png'))
+        self.assertEqual(texture.size, (192, 192))
+
+    def test_texture_array(self):
+        texture = resources.textures.load(
+            TextureDescription(path='textures/array.png', layers=10, kind="array")
+        )
+        self.assertEqual(texture.size, (256, 256, 10))
