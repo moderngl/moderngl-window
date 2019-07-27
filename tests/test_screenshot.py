76c7bda488ceb1fa841d19fd11f16c34890d586d
Einar Forselv <eforselv@gmail.com>
2019-07-27 22:13:22 +0200

    Missing screenshot tests

--- tests/test_screenshot.py
+++ tests/test_screenshot.py
@@ -1,18 +1,30 @@
+from pathlib import Path
 from unittest import mock
-from headless import HeadlessTestCase
-
 from moderngl_window import screenshot
+from headless import HeadlessTestCase
+from utils import settings_context
 
 
 class ScreenshotTestCase(HeadlessTestCase):
 
     @mock.patch('PIL.Image.Image.save', new=mock.MagicMock())
-    def test_create_fbo(self):
+    def test_fbo(self):
         """Create screenshot from fbo"""
         screenshot.create(self.window.fbo)
 
     @mock.patch('PIL.Image.Image.save', new=mock.MagicMock())
-    def test_create_texture(self):
-        """Create screenshot from texture"""        
+    def test_texture(self):
+        """Create screenshot from texture"""
         texture = self.ctx.texture((16, 16), 4)
         screenshot.create(texture)
+
+    @mock.patch('os.makedirs', new=mock.MagicMock())
+    def test_with_screenshot_path(self):
+        """Create screenshot with SCREENSHOT_PATH defined in settings"""
+        with settings_context({'SCREENSHOT_PATH': (Path.cwd() / 'screenshots').resolve()}):
+            self.test_fbo()
+
+    def test_incorrect_source(self):
+        """Attempt to pass invalid source"""
+        with self.assertRaises(ValueError):
+            screenshot.create("Hello")
