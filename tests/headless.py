7769fbaa625ca056e7579c67e6e8b8f0e8a7f33b
Einar Forselv <eforselv@gmail.com>
2019-07-27 19:08:34 +0200

    Test: Extend headless base to include window created from windowconfig

--- tests/headless.py
+++ tests/headless.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 import moderngl
-import moderngl_window
+import moderngl_window as mglw
 from moderngl_window.conf import settings
 
 
@@ -9,6 +9,7 @@ class HeadlessTestCase(TestCase):
     """Test using a headless window/context"""
     window_size = (16, 16)
     aspect_ratio = 1.0
+    gl_version = (4, 1)
 
     @classmethod
     def setUpClass(cls):
@@ -16,11 +17,42 @@ class HeadlessTestCase(TestCase):
         settings.WINDOW['class'] = 'moderngl_window.context.headless.Window'
         settings.WINDOW['size'] = cls.window_size
         settings.WINDOW['aspect_ratio'] = cls.aspect_ratio
+        settings.WINDOW['gl_version'] = cls.gl_version
 
-        cls.window = moderngl_window.create_window_from_settings()
-        moderngl_window.activate_context(window=cls.window)
+        cls.window = mglw.create_window_from_settings()
+        mglw.activate_context(window=cls.window)
 
     @property
     def ctx(self) -> moderngl.Context:
         """moderngl.Context: The active context"""
-        return moderngl_window.ctx()
+        return mglw.ctx()
+
+
+class WindowConfigTestCase(TestCase):
+    """Headless using windowconfig"""
+    TestConfig = None
+    config = None
+    window = None
+
+    @classmethod
+    def setUpClass(cls):
+        window_cls = mglw.get_local_window_cls()
+        cls.window = window_cls(
+            title=cls.TestConfig.title,
+            size=cls.TestConfig.window_size,
+            resizable=cls.TestConfig.resizable,
+            gl_version=cls.TestConfig.gl_version,
+            aspect_ratio=cls.TestConfig.aspect_ratio,
+            samples=cls.TestConfig.samples,
+            cursor=cls.TestConfig.cursor,
+        )
+        cls.config = cls.TestConfig(
+            wnd=cls.window,
+            ctx=cls.window.ctx,
+            timer=None,
+        )
+        cls.window.config = cls.config
+        mglw.activate_context(window=cls.window)
+
+    def ctx(self):
+        return self.window.ctx
