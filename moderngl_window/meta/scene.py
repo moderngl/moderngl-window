c07cf4eec9f787f1ceeef61662d53a43b7f92678
einarf <eforselv@gmail.com>
2019-08-03 11:33:52 +0200

    Default AttributeNames in SceneDescription

--- moderngl_window/meta/scene.py
+++ moderngl_window/meta/scene.py
@@ -1,4 +1,5 @@
 from moderngl_window.meta.base import ResourceDescription
+from moderngl_window.geometry.attributes import AttributeNames
 
 
 class SceneDescription(ResourceDescription):
@@ -6,17 +7,19 @@ class SceneDescription(ResourceDescription):
     default_kind = None
     resource_type = 'scenes'
 
-    def __init__(self, path=None, kind=None, cache=False, **kwargs):
+    def __init__(self, path=None, kind=None, cache=False, attr_names=AttributeNames, **kwargs):
         """Create a scene description
         Keyword Args:
             path (str): Path to resource
             kind (str): Loader kind
             cache (str): Use the loader caching system if present
+            attr_names (AttributeNames): Attrib name config
         """
         kwargs.update({
             "path": path,
             "kind": kind,
             "cache": cache,
+            "attr_names": attr_names,
         })
         super().__init__(**kwargs)
 
