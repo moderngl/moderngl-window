e6a09b3eca40d1a120c67f458a08fe693821ded5
einarf <eforselv@gmail.com>
2019-08-23 23:28:29 +0200

    program.single.Loader docstrings

--- moderngl_window/loaders/program/single.py
+++ moderngl_window/loaders/program/single.py
@@ -12,6 +12,45 @@ class Loader(BaseLoader):
     kind = 'single'
 
     def load(self) -> moderngl.Program:
+        """Loads a shader program from a single glsl file.
+
+        Each shader type is separated by preprocessors
+
+        - VERTEX_SHADER
+        - FRAGMENT_SHADER
+        - GEOMETRY_SHADER
+        - TESS_CONTROL_SHADER
+        - TESS_EVALUATION_SHADER
+
+        Example:
+
+        .. code:: glsl
+
+            #version 330
+
+            #if defined VERTEX_SHADER
+
+            in vec3 in_position;
+            in vec2 in_texcoord_0;
+            out vec2 uv0;
+
+            void main() {
+                gl_Position = vec4(in_position, 1);
+                uv0 = in_texcoord_0;
+            }
+
+            #elif defined FRAGMENT_SHADER
+
+            out vec4 fragColor;
+            uniform sampler2D texture0;
+            in vec2 uv0;
+
+            void main() {
+                fragColor = texture(texture0, uv0);
+            }
+            #endif
+
+        """
         self.meta.resolved_path = self.find_program(self.meta.path)
         if not self.meta.resolved_path:
             raise ImproperlyConfigured("Cannot find program '{}'".format(self.meta.path))
