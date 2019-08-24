9ac2e83d7f560ef98bcb5424e3f71e8956950c06
einarf <eforselv@gmail.com>
2019-08-24 21:55:03 +0200

    DataDescription docstrings

--- moderngl_window/meta/data.py
+++ moderngl_window/meta/data.py
@@ -2,11 +2,40 @@ from moderngl_window.meta.base import ResourceDescription
 
 
 class DataDescription(ResourceDescription):
-    """Describes data file to load"""
+    """Describes data file to load.
+
+    This is a generic resource description type
+    for loading resources that are not textures, programs and scenes.
+    That loaded class is used depends on the ``kind`` or the file extension.
+
+    Currently used to load:
+
+    - text files
+    - json files
+    - binary files
+
+    .. code:: python
+
+        # Describe a text file. Text loader is used based on file extension
+        DataDescription(path='data/text.txt')
+
+        # Describe a json file. Json loader is used based on file extension
+        DataDescription(path='data/data.json')
+
+        # Describe a binary file. Specify a binary loader should be used.
+        DataDescription(path='data/data.bin', kind='binary')
+    """
     default_kind = None
     resource_type = 'data'
 
     def __init__(self, path=None, kind=None, **kwargs):
+        """Initialize the resource description.
+
+        Keyword Args:
+            path (str): Relative path to the resource
+            kind (str): The resource kind deciding loader class
+            **kwargs: Additional custom attributes
+        """
         kwargs.update({
             "path": path,
             "kind": kind,
