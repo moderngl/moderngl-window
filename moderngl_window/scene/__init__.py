68547b08bd5d02b8f38ce9fd00468c8797769c82
einarf <eforselv@gmail.com>
2019-08-25 03:55:19 +0200

    Missing KeyboardCamera import

--- moderngl_window/scene/__init__.py
+++ moderngl_window/scene/__init__.py
@@ -2,6 +2,7 @@
 from .scene import Scene  # noqa
 from .node import Node  # noqa
 from .camera import Camera  # noqa
+from .camera import KeyboardCamera  # noqa
 from .mesh import Mesh  # noqa
 from .material import Material, MaterialTexture  # noqa
 from .programs import MeshProgram  # noqa
