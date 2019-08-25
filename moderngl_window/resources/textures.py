84f8d5d3928958dbbe6f664758adf47f724b5835
einarf <eforselv@gmail.com>
2019-08-25 02:53:44 +0200

    Textures docstrings

--- moderngl_window/resources/textures.py
+++ moderngl_window/resources/textures.py
@@ -8,15 +8,19 @@ from moderngl_window.meta import TextureDescription
 
 
 class Textures(BaseRegistry):
-    """
-    A registry for textures requested by effects.
-    Once all effects are initialized, we ask this class to load the textures.
-    """
+    """Handles texture resources"""
     settings_attr = 'TEXTURE_LOADERS'
 
-    def load(self, meta: TextureDescription) -> Union[moderngl.Texture, moderngl.Texture3D,
-                                                      moderngl.TextureArray, moderngl.TextureCube]:
-        """Loads a texture with the configured loaders"""
+    def load(self, meta: TextureDescription) -> Union[moderngl.Texture, moderngl.TextureArray]:
+        """Loads a texture with the configured loaders.
+
+        Args:
+            meta (:py:class:`~moderngl_window.meta.texture.TextureDescription`): The resource description
+        Returns:
+            moderngl.Texture: 2d texture
+        Returns:
+            moderngl.TextureArray: texture array if ``layers`` is supplied
+        """
         return super().load(meta)
 
 
