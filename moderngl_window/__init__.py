69f85d32e9b5362e931cbd31af63192e4a167400
einarf <eforselv@gmail.com>
2019-10-10 17:41:42 +0200

    Bump version

--- moderngl_window/__init__.py
+++ moderngl_window/__init__.py
@@ -16,7 +16,7 @@ from moderngl_window.timers.clock import Timer
 from moderngl_window.conf import settings
 from moderngl_window.utils.module_loading import import_string
 
-__version__ = '1.5.0'
+__version__ = '1.5.1'
 
 IGNORE_DIRS = [
     '__pycache__',
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="moderngl_window",
-    version="1.5.0",
+    version="1.5.1",
     description="A cross platform helper library for ModernGL making window creation and resource loading simple",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
