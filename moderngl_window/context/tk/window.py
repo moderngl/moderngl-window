7e259131ab2efdcc891623f5f34caf7109b7233b
einarf <eforselv@gmail.com>
2019-10-19 04:42:08 +0200

    tk window: Allow setting window size

--- moderngl_window/context/tk/window.py
+++ moderngl_window/context/tk/window.py
@@ -51,6 +51,21 @@ class Window(BaseWindow):
         self.set_default_viewport()
 
     @property
+    def size(self) -> Tuple[int, int]:
+        """Tuple[int, int]: current window size.
+
+        This property also support assignment::
+
+            # Resize the window to 1000 x 1000
+            window.size = 1000, 1000
+        """
+        return self._width, self._height
+
+    @size.setter
+    def size(self, value: Tuple[int, int]):
+        self._tk.geometry('{}x{}'.format(value[0], value[1]))
+
+    @property
     def position(self) -> Tuple[int, int]:
         """Tuple[int, int]: The current window position.
 
