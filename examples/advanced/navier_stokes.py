889631bfecee76c0ac4af5aba0d3387995d947cd
einarf <eforselv@gmail.com>
2019-11-04 02:52:34 +0100

    example: navier stokes tweaks

--- examples/advanced/navier_stokes.py
+++ examples/advanced/navier_stokes.py
@@ -1,14 +1,13 @@
 """
 GPU version of https://github.com/salt-die/navier-stokes
 """
+import random
 from pathlib import Path
 import numpy as np
 from pyrr import matrix44
 
-import moderngl
 import moderngl_window
 from moderngl_window import geometry
-# from moderngl_window import screenshot
 
 external_flow = .4
 viscosity = .018  # Is it odd that negative viscosity still works?
@@ -17,7 +16,7 @@ viscosity = .018  # Is it odd that negative viscosity still works?
 class NavierStokes2D(moderngl_window.WindowConfig):
     title = "Navier Stokes 2D"
     resource_dir = (Path(__file__) / '../../resources').absolute()
-    window_size = (2000, 720)
+    # window_size = (3440, 1440)
     aspect_ratio = None  # 1280 / 720
     # window_size = 512, 512
     # aspect_ratio = 1.0
@@ -28,8 +27,8 @@ class NavierStokes2D(moderngl_window.WindowConfig):
 
         # various vars
         self.m_proj = matrix44.create_orthogonal_projection(
-            0, self.window_size[0],
-            0, self.window_size[1],
+            0, self.wnd.buffer_width,
+            0, self.wnd.buffer_height,
             -1, 1,
             dtype='f4',
         )
@@ -37,13 +36,17 @@ class NavierStokes2D(moderngl_window.WindowConfig):
 
         # Geometry
         self.quad_fs = geometry.quad_fs()
-        sprite_size = 9 * 8
+        sprite_size = 9 * 4
         self.drop_geometry = geometry.quad_2d(size=(sprite_size, sprite_size))
         self.wall_geometry = geometry.quad_2d(size=(sprite_size, sprite_size))
 
         # Framebuffers
         self.momentum_texture_1 = self.ctx.texture(size, 1, dtype='f4')
+        self.momentum_texture_1.repeat_x = True
+        self.momentum_texture_1.repeat_y = True
         self.momentum_texture_2 = self.ctx.texture(size, 1, dtype='f4')
+        self.momentum_texture_2.repeat_x = True
+        self.momentum_texture_2.repeat_y = True
         self.pressure_texture_1 = self.ctx.texture(size, 1, dtype='f4')
         self.pressure_texture_2 = self.ctx.texture(size, 1, dtype='f4')
         self.walls_texture = self.ctx.texture(size, 1, dtype='f4')
@@ -87,26 +90,32 @@ class NavierStokes2D(moderngl_window.WindowConfig):
         self.pressure_texture_2.write(pressure.tobytes())
 
     def drop(self, x, y):
-        pos = x, self.wnd.buffer_size[1] - y
-        # Render drop into pressure texture
-        self.pressure_fbo_2.use()
-        self.drop_prog['pos'].value = pos
-        self.drop_prog['write_value'].value = 1.0
-        # self.drop.use()
-        self.drop_geometry.render(self.drop_prog)
-        self.momentum_fbo_2.use()
-        self.drop_prog['write_value'].value = 0.0
-        self.drop_geometry.render(self.drop_prog)
+        rng = 40
+        for i in range(10):
+            pos = (
+                x + int((random.random() - 0.5) * rng),
+                self.wnd.buffer_size[1] - y + int((random.random() - 0.5) * rng)
+            )
+            # Render drop into pressure texture
+            self.pressure_fbo_2.use()
+            self.drop_prog['pos'].value = pos
+            self.drop_prog['force'].value = random.random() * 2.0
+            self.drop_prog['write_value'].value = 1.0
+            # self.drop.use()
+            self.drop_geometry.render(self.drop_prog)
+            self.momentum_fbo_2.use()
+            self.drop_prog['write_value'].value = 0.0
+            self.drop_geometry.render(self.drop_prog)
 
     def wall(self, x, y):
-        pos = x, self.wnd.buffer_size[1] - y
+        pos = x, self.wnd.buffer_height - y
         self.walls_fbo.use()
         self.drop_prog['pos'].value = pos
+        self.drop_prog['force'].value = 1.0
         self.drop_prog['write_value'].value = 1.0
         self.drop_geometry.render(self.drop_prog)
 
     def render(self, time, frame_time):
-
         # calculate momentum
         self.momentum_fbo_1.use()
         self.momentum_texture_2.use(location=0)
--- examples/resources/programs/navier-stokes/drop.glsl
+++ examples/resources/programs/navier-stokes/drop.glsl
@@ -19,7 +19,7 @@ void main() {
 
 // uniform sampler2D texture0;
 uniform float write_value;
-const float FORCE = 1.0;
+uniform float force;
 
 out float fragColor;
 in vec2 uv0;
@@ -35,7 +35,7 @@ void main() {
     // }
     vec2 center = uv0 - vec2(0.5);
     float c = step(0.0, 0.5 - length(center));
-    if (c > 0.99) fragColor = write_value;
+    if (c > 0.99) fragColor = write_value * force;
     else discard;
 }
 #endif
