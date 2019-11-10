cf8ed3928263d4cd50a47c49c1578fa99fc19c97
einarf <eforselv@gmail.com>
2019-11-10 20:00:01 +0100

    Examples: CameraWindow - support camera toggle

--- examples/base.py
+++ examples/base.py
@@ -8,15 +8,23 @@ class CameraWindow(mglw.WindowConfig):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.camera = KeyboardCamera(self.wnd.keys, aspect_ratio=self.wnd.aspect_ratio)
+        self.camera_enabled = True
 
     def key_event(self, key, action, modifiers):
-        self.camera.key_input(key, action, modifiers)
+        keys = self.wnd.keys
 
-        if key == self.wnd.keys.SPACE and action == self.wnd.keys.ACTION_PRESS:
-            self.timer.toggle_pause()
+        if self.camera_enabled:
+            self.camera.key_input(key, action, modifiers)
+
+        if action == keys.ACTION_PRESS:
+            if key == keys.C:
+                self.camera_enabled = not self.camera_enabled
+            if key == keys.SPACE:
+                self.timer.toggle_pause()
 
     def mouse_position_event(self, x: int, y: int, dx, dy):
-        self.camera.rot_state(-dx, -dy)
+        if self.camera_enabled:
+            self.camera.rot_state(-dx, -dy)
 
     def resize(self, width: int, height: int):
         self.camera.projection.update(aspect_ratio=self.wnd.aspect_ratio)
