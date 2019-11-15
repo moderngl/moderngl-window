ce57d1058f1007fa3d814908dcad265237e12f74
einarf <eforselv@gmail.com>
2019-11-15 21:37:55 +0100

    Add basic pyopengl example

---
+++ examples/with_pyopengl.py
@@ -0,0 +1,25 @@
+import math
+import moderngl_window
+from OpenGL.GL import *
+
+
+class BasicWindowConfig(moderngl_window.WindowConfig):
+    """Minimal WindowConfig example"""
+    gl_version = (3, 3)
+    title = "Basic Window Config"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def render(self, time, frametime):
+        glClearColor(
+            (math.sin(time) + 1.0) / 2,
+            (math.sin(time + 2) + 1.0) / 2,
+            (math.sin(time + 3) + 1.0) / 2,
+            1.0,
+        )
+        glClear(GL_COLOR_BUFFER_BIT)
+
+
+if __name__ == '__main__':
+    moderngl_window.run_window_config(BasicWindowConfig)
