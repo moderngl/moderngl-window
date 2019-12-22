edcc7b2fc90c67be22ea0126ae6e70f964c31256
Einar Forselv <eforselv@gmail.com>
2019-12-22 11:55:23 +0100

    Example: object rotation

--- examples/advanced/fragment_picking.py
+++ examples/advanced/fragment_picking.py
@@ -3,10 +3,9 @@ from pathlib import Path
 import moderngl
 from pyrr import Matrix44
 import moderngl_window
-from base import CameraWindow
 
 
-class FragmentPicking(CameraWindow):
+class FragmentPicking(moderngl_window.WindowConfig):
     title = "Fragment Picking"
     gl_version = 3, 3
     aspect_ratio = None
@@ -14,21 +13,30 @@ class FragmentPicking(CameraWindow):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.camera.projection.update(near=1.0, far=1000)
-        self.camera.velocity = 100
+
+        # Object rotation
+        self.x_rot = 0
+        self.y_rot = 0
+
         self.scene = self.load_scene('scenes/fragment_picking/centered.obj')
+        self.mesh = self.scene.root_nodes[0]
 
     def render(self, time, frametime):
         self.ctx.enable_only(moderngl.DEPTH_TEST | moderngl.CULL_FACE)
 
-        model = Matrix44.from_translation((0, 0, -50))
-        camera = self.camera.matrix
-        modelview = camera * model
+        projection = Matrix44.perspective_projection(60, self.wnd.aspect_ratio, 1, 100, dtype='f4')
+        translation = Matrix44.from_translation((0, 0, -50))
+        rotation = Matrix44.from_eulers((self.y_rot, self.x_rot, 0))
+        modelview = translation * rotation
 
         self.scene.draw(
-            self.camera.projection.matrix,
+            projection,
             modelview,
         )
+
+    def mouse_drag_event(self, x, y, dx, dy):
+        self.x_rot -= dx / 100
+        self.y_rot -= dy / 100
 
 
 if __name__ == '__main__':
