5847675189f3381affe08d72948994ccaf9d773e
Fabrice Rosenek <f.rosenek@gmail.com>
2020-01-04 02:52:03 +0100

    Initialize reverse key map and imgui key map

--- moderngl_window/integrations/imgui.py
+++ moderngl_window/integrations/imgui.py
@@ -7,7 +7,6 @@ from imgui.integrations.opengl import BaseOpenGLRenderer
 
 
 class ModernglWindowMixin:
-    REVERSE_KEY_MAP = {}
 
     def resize(self, width: int, height: int):
         self.io.display_size = (
@@ -215,6 +214,35 @@ class ModernglWindowRenderer(ModernglRenderer, ModernglWindowMixin):
     def __init__(self, window):
         super().__init__(ctx=window.ctx)
         self.wnd = window
-
+        self._init_key_maps()
         self.io.display_size = self.wnd.size
         self.io.display_fb_scale = 1.0, 1.0
+
+    def _init_key_maps(self):
+        keys = self.wnd.keys
+
+        self.REVERSE_KEY_MAP = {
+            keys.TAB: imgui.KEY_TAB,
+            keys.LEFT: imgui.KEY_LEFT_ARROW,
+            keys.RIGHT: imgui.KEY_RIGHT_ARROW,
+            keys.UP: imgui.KEY_UP_ARROW,
+            keys.DOWN: imgui.KEY_DOWN_ARROW,
+            keys.PAGE_UP: imgui.KEY_PAGE_UP,
+            keys.PAGE_DOWN: imgui.KEY_PAGE_DOWN,
+            keys.HOME: imgui.KEY_HOME,
+            keys.END: imgui.KEY_END,
+            keys.DELETE: imgui.KEY_DELETE,
+            keys.SPACE: imgui.KEY_SPACE,
+            keys.BACKSPACE: imgui.KEY_BACKSPACE,
+            keys.ENTER: imgui.KEY_ENTER,
+            keys.ESCAPE: imgui.KEY_ESCAPE,
+            keys.A: imgui.KEY_A,
+            keys.C: imgui.KEY_C,
+            keys.V: imgui.KEY_V,
+            keys.X: imgui.KEY_X,
+            keys.Y: imgui.KEY_Y,
+            keys.Z: imgui.KEY_Z,
+        }
+
+        for value in self.REVERSE_KEY_MAP.values():
+            self.io.key_map[value] = value
