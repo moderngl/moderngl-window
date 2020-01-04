861d3892138b877ef39ec6a03c9d80d4570aedd1
Einar Forselv <eforselv@gmail.com>
2020-01-04 03:43:19 +0100

    Update shadow_mapping.py

--- examples/advanced/shadow_mapping.py
+++ examples/advanced/shadow_mapping.py
@@ -1,3 +1,7 @@
+"""
+Shadow mapping example from:
+https://www.opengl-tutorial.org/intermediate-tutorials/tutorial-16-shadow-mapping/
+"""
 from pathlib import Path
 import moderngl_window
 
@@ -10,6 +14,14 @@ class ShadowMapping(moderngl_window.WindowConfig):
         super().__init__(**kwargs)
 
         self.linearize_depth_program = self.load_program('programs/linearize_depth.glsl')
+
+        # Offscreen buffer
+        self.offscreen_depth = self.ctx.depth_texture(self.wnd.buffer_size)
+        self.offscreen_color = self.ctx.texture(self.wnd.buffer_size, 4)
+        self.offscreen = self.ctx.framebuffer(
+            color_attachments=[self.offscreen_color],
+            depth_attachment=self.offscreen_depth,
+        )
 
     def render(self, time, frametime):
         pass
