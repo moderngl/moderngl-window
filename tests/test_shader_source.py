ab38a1fab0c6bdd3519f9559fcceb611bc15532d
Einar Forselv <eforselv@gmail.com>
2020-05-09 15:08:06 +0200

    Tests: injecting defines in compute shader

--- tests/test_shader_source.py
+++ tests/test_shader_source.py
@@ -77,6 +77,36 @@ class ShaderSourceTestCase(TestCase):
         self.assertTrue("#define NUM_THINGS 100" in shader.source)
         self.assertTrue("#define SCALE 2.0" in shader.source)
 
+    def test_define_compute(self):
+        """Injecting defines in compute shader"""
+        shader = program.ShaderSource(
+            program.COMPUTE_SHADER,
+            "compute.glsl",
+            """
+            #version 430
+            #define TEST 0
+            #define THING 0
+            layout (local_size_x = 1, local_size_y = 1) in;
+            layout (std430, binding = 1) buffer Input {
+                float v1[4];
+            };
+            layout (std430, binding = 2) buffer Output {
+                float v2[4];
+            };
+            uniform float mul;
+            uniform vec4 add;
+            void main() {
+                v2[0] = v1[3] * mul + add.x;
+                v2[1] = v1[2] * mul + add.y;
+                v2[2] = v1[1] * mul + add.z;
+                v2[3] = v1[0] * mul + add.w;
+            }
+            """,
+            defines={'TEST': 12, 'THING': '23'},
+        )
+        self.assertTrue("#define TEST 12" in shader.source)
+        self.assertTrue("#define THING 23" in shader.source)
+
     def test_include(self):
         """Test #include preprocessors (recursive)"""
         def load_source(path):
