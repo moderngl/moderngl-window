738dc490d2760a88206839cb4a6c2be213b1fc37
Einar Forselv <eforselv@gmail.com>
2021-06-23 14:47:12 +0200

    Add text example

---
+++ examples/text_simple.py
@@ -0,0 +1,18 @@
+import moderngl_window
+from moderngl_window.text.bitmapped import TextWriter2D
+
+
+class App(moderngl_window.WindowConfig):
+    title = "Text"
+    aspect_ratio = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.writer = TextWriter2D()
+        self.writer.text = "Hello ModernGL!"
+
+    def render(self, time, frame_time):
+        self.writer.draw((240, 380), size=120)
+
+
+App.run()
