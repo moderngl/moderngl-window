e1839ed0fa16eca1acb0834bef975e6926de3817
Einar Forselv <eforselv@gmail.com>
2021-12-26 00:50:18 +0100

    Re-organize simple atlas

--- moderngl_window/atlas/simple_atlas.py
+++ moderngl_window/atlas/simple_atlas.py
@@ -14,63 +14,6 @@ import moderngl
 from .base import BaseImage
 
 
-class SimpleAtlas:
-    """
-    A simple RGBA texture atlas using a row based allocation.
-    There are more efficient ways to pack textures, but this
-    is normally sufficient for dynamic atlases were textures
-    are added on the fly runtime.
-    """
-    def __init__(self, ctx: moderngl.Context, width: int, height: int, border: int = 1):
-        # Basic properties
-        self._ctx = ctx
-        self._width = width
-        self._height = height
-        # Atlas content
-        self._texture = self._ctx.texture(self.size, components=4)
-        # We want to be able to render into the atlas texture
-        self._fbo = self._fbo = self._ctx.framebuffer(
-            color_attachments=[self._texture]
-        )
-
-    @property
-    def ctx(self) -> moderngl.Context:
-        """The moderngl contex this atlas belongs to"""
-        return self._ctx
-
-    @property
-    def textrue(self) -> moderngl.Texture:
-        """The moderngl texture with the atlas contents"""
-        return self._texture
-
-    @property
-    def width(self) -> int:
-        """int: Width of the atlas in pixels"""
-        return self._width
-
-    @property
-    def height(self) -> int:
-        """int: Height of the atlas in pixels"""
-        return self._height
-
-    @property
-    def size(self) -> Tuple[int, int]:
-        """Tuple[int, int]: The size of he atlas (width, height)"""
-        return self._width, self._height
-
-    def add(self, image: BaseImage):
-        pass
-
-    def remove(self, image: BaseImage):
-        pass
-
-    def resize(self, width: int, height: int):
-        pass
-
-    def rebuild(self):
-        pass
-
-
 class AllocatorException(Exception):
     pass
 
@@ -144,3 +87,84 @@ class Allocator:
             return new_row.add(width, height)
 
         raise AllocatorException("No more space in {} for box [{}, {}]".format(self, width, height))
+
+
+class TextureAtlas:
+    """
+    A simple texture atlas using a row based allocation.
+    There are more efficient ways to pack textures, but this
+    is normally sufficient for dynamic atlases were textures
+    are added on the fly runtime.
+    """
+    def __init__(
+        self,
+        ctx: moderngl.Context,
+        width: int,
+        height: int,
+        components: int = 4,
+        border: int = 1,
+        auto_resize: bool = True,
+    ):
+        # Basic properties
+        self._ctx = ctx
+        self._width = width
+        self._height = height
+        self._components = components
+        self._border = border
+        self._auto_resize = auto_resize
+
+        # The physical size limit for the current hardware
+        self._max_size = self._ctx.info["GL_MAX_VIEWPORT_DIMS"]
+
+        # Atlas content
+        self._texture = self._ctx.texture(self.size, components=self._components)
+
+        # We want to be able to render into the atlas texture
+        self._fbo = self._fbo = self._ctx.framebuffer(
+            color_attachments=[self._texture]
+        )
+        self._allocator = Allocator(width, height)
+
+    @property
+    def ctx(self) -> moderngl.Context:
+        """The moderngl contex this atlas belongs to"""
+        return self._ctx
+
+    @property
+    def textrue(self) -> moderngl.Texture:
+        """The moderngl texture with the atlas contents"""
+        return self._texture
+
+    @property
+    def width(self) -> int:
+        """int: Width of the atlas in pixels"""
+        return self._width
+
+    @property
+    def height(self) -> int:
+        """int: Height of the atlas in pixels"""
+        return self._height
+
+    @property
+    def size(self) -> Tuple[int, int]:
+        """Tuple[int, int]: The size of he atlas (width, height)"""
+        return self._width, self._height
+
+    @property
+    def max_size(self) -> Tuple[int, int]:
+        """
+        Tuple[int,int]: The maximum size of the atlas in pixels (x, y)
+        """
+        return self._max_size
+
+    def add(self, image: BaseImage):
+        pass
+
+    def remove(self, image: BaseImage):
+        pass
+
+    def resize(self, width: int, height: int):
+        pass
+
+    def rebuild(self):
+        pass
