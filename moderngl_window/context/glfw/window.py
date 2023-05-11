b5fe1b6c428ea5b3092f3bd8e8d46ea5f3ce0648
Einar Forselv <eforselv@gmail.com>
2023-05-11 19:40:57 +0200

    Add stencil bits to glfw window

--- moderngl_window/context/glfw/window.py
+++ moderngl_window/context/glfw/window.py
@@ -40,6 +40,7 @@ class Window(BaseWindow):
         glfw.window_hint(glfw.RESIZABLE, self.resizable)
         glfw.window_hint(glfw.DOUBLEBUFFER, True)
         glfw.window_hint(glfw.DEPTH_BITS, 24)
+        glfw.window_hint(glfw.STENCIL_BITS, 8)
         glfw.window_hint(glfw.SAMPLES, self.samples)
         glfw.window_hint(glfw.SCALE_TO_MONITOR, glfw.TRUE)
 
